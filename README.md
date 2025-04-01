3f1b68c449786184e89e267c40b16d25bb458629
Anyeli Tamara <anyelitamara1@gmail.com>
2025-04-01 00:02:59 -0500

    Nuevo par de claves

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 Primer Cambio: Crear un archivo main.py
 Segundo cambio: Añadir linea al archivo main.py
+Tercer cambio:Par de claves SSH creadas
+
--- main.py
+++ main.py
@@ -1,4 +1,4 @@
 print("Este es mi primer proyecto con Git")
-print(Otro cambio para mi tercer commit de mi repositorio con Git)
+print("Otro cambio para mi tercer commit de mi repositorio con Git")
 print("Hola mundo")
-
+print("Ya tengo el par de mi clave ssh")
